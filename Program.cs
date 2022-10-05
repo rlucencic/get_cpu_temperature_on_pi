d155f3ac5c4fcffdd44a39b7c54ceafbe41a7373
Robert Lucencic <robert.lucencic@gmail.com>
2022-10-05 21:04:57 +0200

    Promjene

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Threading;
+using Iot.Device.CpuTemperature;
+
+using var cpuTemperature = new CpuTemperature();
+Console.WriteLine("Press any key to quit");
+
+while (!Console.KeyAvailable) {
+	if (cpuTemperature.IsAvailable) {
+		var temperature = cpuTemperature.ReadTemperatures();
+		foreach (var entry in temperature) {
+			if (!double.IsNaN(entry.Temperature.DegreesCelsius)) {
+				Console.WriteLine($"Temperature from {entry.Sensor}: {entry.Temperature.DegreesCelsius} °C");
+			}
+			else
+			{
+				Console.WriteLine("Unable to read temperature.");
+			}
+		}
+	}
+	else 
+	{
+		Console.WriteLine("CPU temperature is not available");
+	}
+	Thread.Sleep(1000);
+}
